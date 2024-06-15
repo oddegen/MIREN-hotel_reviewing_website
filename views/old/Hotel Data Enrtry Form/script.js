79a80b0594d3349e0afb923816a81e0f144cd501
oddegen <nahomtensae@gmail.com>
2024-06-15 16:52:49 +0300

    changes changes changes

--- views/old/Hotel Data Enrtry Form/script.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector("#form");
-
-form.addEventListener("submit", () => {
-  setTimeout(() => {
-    window.location.href = "../Home page/home.html";
-  }, 2000);
-});
